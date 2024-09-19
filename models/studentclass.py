f0ab99319c812bf69606b97a2954471ca86587c3
Vastav Taneja <vastav.taneja@targetintegration.com>
2024-09-19 12:24:31 +0530

    final

--- models/studentclass.py
+++ models/studentclass.py
@@ -1,10 +1,11 @@
 from odoo import models, fields,api
-
+import logging
 class Studentclass(models.Model):
     _name = 'school.studentclass'
     _description = 'Students in Class'
 
     name = fields.Char(string='Class Name', required=True)
+    email=fields.Char(string="School Email",readonly=True)
     strength = fields.Integer(string='Class Strength',readonly=True,compute="_compute_class_strength")
     status = fields.Selection([('active', 'Active'), ('inactive', 'Inactive')], string='Status', default='active')
     student_ids=fields.One2many('school.student','class_id',string="Students in Class",readonly=True)
@@ -17,7 +18,22 @@ class Studentclass(models.Model):
         string="Subjects"
     )
     
-    
+    @api.onchange('name')
+    def compute_email(self):
+        _logger=logging.getLogger(__name__)
+        _logger.info(f"self {self}")
+        _logger.info(f"name-------- {self.name}")
+        if self.name:
+            self.email=self.name+"@gmail.com"
+        _logger.info(f"self.id {type(self.id)}")
+        record_id_str = f"{self.id}"  # e.g., 'NewId_6'
+
+        # Extract the numeric part (assuming it's the last part after an underscore)
+        record_id = int(record_id_str.split('_')[-1])
+        _logger.info(f"id-------- {record_id}")
+        if record_id:
+            record=self.env['school.studentclass'].browse(record_id)
+            record.email=self.email
     @api.depends('student_ids')
     def _compute_class_strength(self):
         for record in self:
